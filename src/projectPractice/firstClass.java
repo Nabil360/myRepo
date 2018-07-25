d73c7d7cadfb1df8e58cc711837042688e41fe15
DESKTOP-QQV8890\Nabil <abirahmed666@gmail.com>
2018-07-24 20:32:06 -0500

    new build

---
+++ src/projectPractice/firstClass.java
@@ -0,0 +1,34 @@
+package projectPractice;
+import java.util.*;
+public class firstClass {
+
+	public static void main(String[] args) {
+		
+		
+		int nameOfArray[]=new int[3];
+		
+		nameOfArray[0]=1;
+		nameOfArray[1]=2;
+		nameOfArray[2]=3;
+		
+		
+		
+		int nameOfArray1[]= {1,2,3,4,5,6};
+		
+		
+		for(int i=0;i<3;i++) {
+			System.out.println(nameOfArray[i]);
+		}
+
+
+		for(int i=0;i<6;i++) {
+			System.out.println(nameOfArray1[i]);
+			
+		
+		}
+
+
+		
+
+	}
+}
